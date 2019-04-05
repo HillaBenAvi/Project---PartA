f53ab246043dd90167b96edb63a8f73dc66fe023
Hilla <hillabe@post.bgu.ac.il>
2019-04-05 17:12:48 +0300

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       // MyMazeGenerator check = new MyMazeGenerator();
+        //long res = check.measureAlgorithmTimeMillis(10, 10);
+        //System.out.println(res);
+       // long start = System.currentTimeMillis();
+       // Maze checkMaze = check.generate(20,20);
+      //  long end = System.currentTimeMillis();
+      //  System.out.println(end-start);
+     //   checkMaze.print();
+        //checkMaze.Print();
+
+
+    }
+}
